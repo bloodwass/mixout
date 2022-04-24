9f0322e54a0428f44834e7baaa32bc47977c8c5a
vad13irt <w13vadirt@gmail.com>
2022-04-24 21:13:31 +0300

    deleted test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import torch
-from torch import nn
-from mixout import MixLinear
-
-class Model(nn.Module):
-    def __init__(self):
-        super(Model, self).__init__()
-        self.linear = MixLinear(in_features=1, out_features=1, p=0.4)
